30ec202526836b0bef25c42a75bc63c7a128dd6c
Andrés Alin <andreas.alin@gmail.com>
2026-07-08 23:25:42 -0500

    Expand Haml backtrace coverage

--- lib/klenod/build/plugins/haml_plugin.test.rb
+++ lib/klenod/build/plugins/haml_plugin.test.rb
@@ -1176,6 +1176,115 @@ class Klenod::Build::Plugins::HamlPlugin::Test < Minitest::Test
     end
   end
 
+  def test_default_haml_transformer_rewrites_dynamic_attribute_errors_to_haml_lines
+    Dir.mktmpdir do |dir|
+      FileUtils.mkdir_p("#{dir}/pages")
+      File.write(
+        "#{dir}/pages/page.haml",
+        <<~HAML
+          %main{ class: (raise "class boom") }
+            %h1 Hello
+        HAML
+      )
+
+      plugin =
+        Klenod::Build::Plugins::HamlPlugin.new(
+          factory: "#{self.class.name}::FakeFramework::H"
+        )
+      context = Klenod::Build::Context.new(source_dir: dir, plugins: default_plugins_with(plugin))
+      record = context.load("pages/page.haml")
+      mod = context.graph.mods.fetch(record.id)
+      exports = mod.const_get(:Exports)
+
+      error = assert_raises(RuntimeError) { exports::Default.new.render }
+      Klenod::BacktraceRewriter.new({"pages/page.haml" => mod}).rewrite_exception(error)
+
+      assert_match(/\Apages\/page\.haml:1:in /, error.backtrace.fetch(0))
+    end
+  end
+
+  def test_default_haml_transformer_rewrites_ruby_filter_method_called_from_markup
+    Dir.mktmpdir do |dir|
+      FileUtils.mkdir_p("#{dir}/pages")
+      File.write(
+        "#{dir}/pages/page.haml",
+        <<~HAML
+          :ruby
+            def title
+              raise "title boom"
+            end
+
+          %main
+            %h1= title
+        HAML
+      )
+
+      plugin =
+        Klenod::Build::Plugins::HamlPlugin.new(
+          factory: "#{self.class.name}::FakeFramework::H"
+        )
+      context = Klenod::Build::Context.new(source_dir: dir, plugins: default_plugins_with(plugin))
+      record = context.load("pages/page.haml")
+      mod = context.graph.mods.fetch(record.id)
+      exports = mod.const_get(:Exports)
+
+      error = assert_raises(RuntimeError) { exports::Default.new.render }
+      Klenod::BacktraceRewriter.new({"pages/page.haml" => mod}).rewrite_exception(error)
+
+      assert_match(/\Apages\/page\.haml:3:in /, error.backtrace.fetch(0))
+    end
+  end
+
+  def test_default_haml_transformer_rewrites_imported_component_render_errors_to_component_haml
+    Dir.mktmpdir do |dir|
+      FileUtils.mkdir_p("#{dir}/components")
+      File.write(
+        "#{dir}/components/details.haml",
+        <<~HAML
+          :ruby
+            def initialize(children: nil)
+            end
+
+          %section
+            %h2 Details
+            = raise "details boom"
+        HAML
+      )
+      File.write(
+        "#{dir}/page.haml",
+        <<~HAML
+          :ruby
+            Details = import("components/details.haml")
+
+          %main
+            %Details
+        HAML
+      )
+
+      plugin =
+        Klenod::Build::Plugins::HamlPlugin.new(
+          factory: "#{self.class.name}::FakeFramework::H"
+        )
+      context = Klenod::Build::Context.new(source_dir: dir, plugins: default_plugins_with(plugin))
+      page_record = context.load("page.haml")
+      page_mod = context.graph.mods.fetch(page_record.id)
+      details_mod = context.graph.mods.fetch(ModuleId.new("components/details.haml", nil))
+      exports = page_mod.const_get(:Exports)
+
+      error = assert_raises(RuntimeError) { exports::Default.new.render }
+      Klenod::BacktraceRewriter
+        .new(
+          {
+            "page.haml" => page_mod,
+            "components/details.haml" => details_mod
+          }
+        )
+        .rewrite_exception(error)
+
+      assert_match(/\Acomponents\/details\.haml:7:in /, error.backtrace.fetch(0))
+    end
+  end
+
   def test_haml_uses_custom_transformer_contract
     Dir.mktmpdir do |dir|
       FileUtils.mkdir_p("#{dir}/pages")
