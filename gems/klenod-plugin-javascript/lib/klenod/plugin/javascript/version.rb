1429c6119d584cfe8744c0505f5f6514a37ad943
Andrés Alin <andreas.alin@gmail.com>
2026-09-05 18:54:14 -0500

    Bump version to 0.0.6

--- gems/klenod-plugin-javascript/lib/klenod/plugin/javascript/version.rb
+++ gems/klenod-plugin-javascript/lib/klenod/plugin/javascript/version.rb
@@ -4,7 +4,7 @@ module Klenod
   module Build
     module Plugins
       module JavaScriptPlugin
-        VERSION = "0.0.5"
+        VERSION = "0.0.6"
       end
     end
   end
--- gems/klenod-test/lib/klenod/test/version.rb
+++ gems/klenod-test/lib/klenod/test/version.rb
@@ -2,6 +2,6 @@
 
 module Klenod
   module Test
-    VERSION = "0.0.5"
+    VERSION = "0.0.6"
   end
 end
