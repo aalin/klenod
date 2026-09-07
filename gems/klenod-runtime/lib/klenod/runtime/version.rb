09a348391534f96c10923c3d90f129c100dab20b
Andrés Alin <andreas.alin@gmail.com>
2026-09-07 10:26:27 -0500

    Bump version to 0.0.9

--- gems/klenod-runtime/lib/klenod/runtime/version.rb
+++ gems/klenod-runtime/lib/klenod/runtime/version.rb
@@ -2,6 +2,6 @@
 
 module Klenod
   module Runtime
-    VERSION = "0.0.8"
+    VERSION = "0.0.9"
   end
 end
--- gems/klenod-test/lib/klenod/test/version.rb
+++ gems/klenod-test/lib/klenod/test/version.rb
@@ -2,6 +2,6 @@
 
 module Klenod
   module Test
-    VERSION = "0.0.8"
+    VERSION = "0.0.9"
   end
 end
