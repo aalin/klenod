1f8720c5cb00cb0edc104da719867d16ff6fda58
Andrés Alin <andreas.alin@gmail.com>
2026-09-11 20:02:08 -0500

    Bump version to 0.0.14

--- gems/klenod-plugin-javascript/lib/klenod/plugin/javascript/version.rb
+++ gems/klenod-plugin-javascript/lib/klenod/plugin/javascript/version.rb
@@ -4,7 +4,7 @@ module Klenod
   module Build
     module Plugins
       module JavaScriptPlugin
-        VERSION = "0.0.13"
+        VERSION = "0.0.14"
       end
     end
   end
--- gems/klenod-runtime/lib/klenod/runtime/version.rb
+++ gems/klenod-runtime/lib/klenod/runtime/version.rb
@@ -2,6 +2,6 @@
 
 module Klenod
   module Runtime
-    VERSION = "0.0.13"
+    VERSION = "0.0.14"
   end
 end
