788e8209e4ef4ab2fca08dabbd5bb7ed1ce7a01e
Andrés Alin <andreas.alin@gmail.com>
2026-09-13 21:55:31 -0500

    Bump version to 0.0.16

--- gems/klenod-plugin-css/lib/klenod/plugin/css/version.rb
+++ gems/klenod-plugin-css/lib/klenod/plugin/css/version.rb
@@ -4,7 +4,7 @@ module Klenod
   module Build
     module Plugins
       module CSSPlugin
-        VERSION = "0.0.15"
+        VERSION = "0.0.16"
       end
     end
   end
--- gems/klenod-test/lib/klenod/test/version.rb
+++ gems/klenod-test/lib/klenod/test/version.rb
@@ -2,6 +2,6 @@
 
 module Klenod
   module Test
-    VERSION = "0.0.15"
+    VERSION = "0.0.16"
   end
 end
